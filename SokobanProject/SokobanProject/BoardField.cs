e33c0049872ffe19016a8cd7154fbbee4b5d5b27
Daniel Greibe <Danielgreibe@gmail.com>
2019-03-20 12:55:55 +0100

    Added SokobanGame Logic to the project.

---
+++ SokobanProject/SokobanProject/BoardField.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SokobanLocal
+{
+    public abstract class BoardField
+    {
+        public abstract override string ToString();
+        public abstract Boolean PlayerIsHere { get; set; }
+        public abstract Boolean BoxIsHere { get; set; }
+        public abstract Boolean IsGoalField { get; set; }
+        public abstract Boolean IsWallField { get; set; }
+    }
+}
---
+++ SokobanProject/SokobanProject/Game.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SokobanLocal
+{
+    class Game
+    {
+        public static void PlayGame()
+        {
+            Boolean GameWon = false;
+            GameLevel gamelevel1 = new GameLevel(1);
+            gamelevel1.PrintBoard();
+            while (GameWon == false)
+            {
+                Console.WriteLine("W to go Up, A to go Left, S to go Down and D to go Right");
+                ConsoleKey direction = Console.ReadKey().Key;
+                Console.Clear();
+                switch (direction)
+                {
+                    case ConsoleKey.UpArrow: 
+                    case ConsoleKey.W : gamelevel1.Move(0);
+                        break;
+                    case ConsoleKey.LeftArrow:
+                    case ConsoleKey.A: gamelevel1.Move(1);
+                        break;
+                    case ConsoleKey.DownArrow:
+                    case ConsoleKey.S: gamelevel1.Move(2);
+                        break;
+                    case ConsoleKey.RightArrow:
+                    case ConsoleKey.D: gamelevel1.Move(3);
+                        break;
+                    default:
+                        Console.WriteLine("Didn't understand input");
+                        break;
+                }
+                GameWon = gamelevel1.IsGameWon();
+                gamelevel1.PrintBoard();
+            }
+            Console.WriteLine("Congratulations, you won the game");
+        }
+    }
+}
