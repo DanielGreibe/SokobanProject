6fea818d24c934dc8dbd68f643dbbfdfdcc1d5d2
Hoegsgaard <mathiashoegsgaard@gmail.com>
2019-03-20 14:11:41 +0100

    fejl fikset, håber jeg

--- SokobanProject/SokobanProject/Models/ErrorViewModel.cs
+++ SokobanProject/SokobanProject/Models/ErrorViewModel.cs
@@ -2,8 +2,7 @@ using System;
 
 namespace SokobanProject.Models
 {
-    public class ErrorViewModel
-    {
+    public class ErrorViewModel{
         public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
